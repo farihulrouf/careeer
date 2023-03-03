6cb4420f133cd3ec4076b42f85b5b8e30f50d992
Farihul Rouf <farihulrouf@gmail.com>
2023-03-03 19:44:22 +0700

    shadow

--- src/pages/CareerPath/Career.js
+++ src/pages/CareerPath/Career.js
@@ -1,6 +1,7 @@
 import React, { Component, useEffect } from "react";
 import ProgressBar from "../../components/chart/ProgressBar";
-import { AiOutlineFilter, AiOutlineFilePdf, AiOutlineTrophy } from "react-icons/ai";
+import { AiOutlineFilter, AiOutlineFilePdf, AiFillGold, AiOutlineTrophy, AiOutlineUsergroupDelete } from "react-icons/ai";
+import { BiRun } from "react-icons/bi"
 import { Chart } from "react-google-charts";
 import Dropdown from 'react-multilevel-dropdown';
 import DataTable from 'react-data-table-component';
@@ -9,30 +10,30 @@ import DataTable from 'react-data-table-component';
 
 const customStyles = {
   rows: {
-      style: {
-          minHeight: '50px', // override the row height
-      },
+    style: {
+      minHeight: '50px', // override the row height
+    },
   },
   headCells: {
-      style: {
-          paddingLeft: '8px', // override the cell padding for head cells
-          paddingRight: '8px',
-          fontSize: '16px',
-        
-      },
+    style: {
+      paddingLeft: '8px', // override the cell padding for head cells
+      paddingRight: '8px',
+      fontSize: '16px',
+
+    },
   },
   cells: {
-      style: {
-          paddingLeft: '8px', // override the cell padding for data cells
-          paddingRight: '8px',
-          fontSize: '14px',
+    style: {
+      paddingLeft: '8px', // override the cell padding for data cells
+      paddingRight: '8px',
+      fontSize: '14px',
 
-      },
+    },
   },
 };
 
 export const data = [
-  ["Covered", "Skill Gap"],
+  ["Skill Gap Covered", "Skill Gap"],
   ["Covered", 40],
   ["Skill Gap", 60],
 ];
@@ -72,7 +73,7 @@ export const dataBar = [
 ];
 
 export const optionsBar = {
- 
+
   vAxis: { title: "Y-Axis" },
   hAxis: { title: "X-Axis" },
   seriesType: "bars",
@@ -82,50 +83,50 @@ export const optionsBar = {
 
 const columnstable = [
   {
-      name: 'Department Name',
-      selector: row => row.title,
+    name: 'Department Name',
+    selector: row => row.title,
   },
   {
-      name: 'Leader Board PTS',
-      selector: row => row.leader,
+    name: 'Leader Board PTS',
+    selector: row => row.leader,
   },
   {
     name: 'Position Moved',
     selector: row => row.position,
-},
+  },
 ];
 
 const datatable = [
   {
-      id: 1,
-      title: 'TAL-CS',
-      leader: '486',
-      position: '20 PTS'
+    id: 1,
+    title: 'TAL-CS',
+    leader: '486',
+    position: '20 PTS'
   },
   {
-      id: 2,
-      title: 'TAL-CS',
-      leader: '486',
-      position: '20 PTS'
+    id: 2,
+    title: 'TAL-CS',
+    leader: '486',
+    position: '20 PTS'
   },
   {
     id: 3,
     title: 'TAL-CS',
     leader: '486',
     position: '20 PTS'
-},
+  },
   {
-      id: 4,
-      title: 'TAL-CS',
-      leader: '486',
-      position: '20 PTS'
+    id: 4,
+    title: 'TAL-CS',
+    leader: '486',
+    position: '20 PTS'
   },
   {
     id: 5,
     title: 'TAL-TX',
     leader: '486',
     position: '20 PTS'
-},
+  },
 ]
 
 
@@ -140,80 +141,91 @@ const Career = () => {
           <div>
             <h1 className="text-xl text-white bg-indigo-700 px-2 py-2 w-52 rounded-sm">Analytics Dashboard</h1>
           </div>
-          <div className="mb-2 w-full flex">
-            <div className="w-1/3 justify-center gap-4 items-center flex">
-             
-            </div>
-            <div className="w-2/3 flex justify-end space-x-1">
-              <div className="text-center font-semibold flex items-center">
-                <ul className="bg-teal-500 text-white p-2 hover:text-white hover:bg-indigo-500">
-                  <li className="text-lg">800</li>
-                  <li>Total Users</li>
-                </ul>
-              </div>
+          <div className="mb-2 w-full justify-between flex">
+            <div>
+              <div className="flex space-x-2">
+                <div className="text-center font-semibold flex items-center">
+                  <ul className="w-44 relative drop-shadow-lg rounded-md bg-gradient-to-r from-yellow-400 bg-pink-300 p-2 hover:text-white hover:bg-indigo-500">
+                    <p className="absolute text-white text-lg top-2/3 transform -translate-y-1/2">26</p>
+                    <li className="text-black text-xl">Total Employees</li>
+                    <li className="text-xl text-black">800</li>
+                    <div className="absolute  text-white text-md bottom-1 right-1">
+                      <AiOutlineUsergroupDelete size={24} />
+                    </div>
+                  </ul>
+                </div>
 
-              <div className="text-center font-semibold flex items-center">
-                <ul className="bg-pink-500 text-white p-2 hover:text-white hover:bg-indigo-500">
-                  <li className="text-lg">200</li>
-                  <li>Exit Employees</li>
-                </ul>
+                <div className="text-center font-semibold flex items-center">
+                  <ul className="w-44 relative drop-shadow-lg rounded-md bg-gradient-to-r from-pink-300 bg-pink-500 text-white p-2 hover:text-white hover:bg-indigo-500">
+                    <p className="absolute text-white text-lg top-2/3 transform -translate-y-1/2">4</p>
+                    <li className="text-black text-xl">Exit Employees</li>
+                    <li className="text-xl text-black">200</li>
+                    <div className="absolute  text-white text-md bottom-1 right-1">
+                      <BiRun size={24} />
+                    </div>
+                  </ul>
+                </div>
+                <div className="text-center font-semibold flex items-center">
+                  <ul className="w-44 relative drop-shadow-lg rounded-md bg-gradient-to-r from-green-200 bg-green-500 text-white p-2 hover:text-white hover:bg-indigo-500">
+                    <p className="absolute text-white text-lg top-2/3 transform -translate-y-1/2">5</p>
+                    <li className="text-black text-xl">Attrition</li>
+                    <li className="text-xl text-black">10%</li>
+
+                    <div className="absolute  text-white text-md bottom-1 right-1">
+                      <AiFillGold size={24} />
+                    </div>
+                  </ul>
+                </div>
               </div>
-              <div className="text-center font-semibold flex items-center">
-                <ul className="bg-orange-500 text-white p-2 hover:text-white hover:bg-indigo-500">
-                  <li className="text-lg">10%</li>
-                  <li>Attrition</li>
-                </ul>
-              </div>
-              <div className="text-left font-semibold flex items-center">
-                <ul className="bg-indigo-700 text-white p-2 hover:text-white hover:bg-indigo-500">
-                  <li className="text-lg">Top Dept On Leader board</li>
-                  <li className="relative">
-                    Cient Services <span className="text-red-500 ml-2">486</span>
-                    <AiOutlineTrophy className="text-pink-700 absolute right-0 top-0"  size={28}/>
-                  </li>
-                </ul>
-              </div>
-             
+            </div>
+
+            <div className="text-left font-semibold flex items-center">
+              <ul className="bg-red-400 drop-shadow-lg  rounded-md p-2 hover:text-white hover:bg-indigo-500">
+                <li className="text-xl text-black">Top Dept On Leader board</li>
+                <li className="relative text-lg">
+                  Cient Services <span className="ml-2 text-black text-xl">486</span>
+                </li>
+              </ul>
             </div>
           </div>
           <div className="w-full flex justify-end">
             <div className="flex space-x-2 mb-1">
               <div className="bg-indigo-700 text-white flex space-x-2 px-1 items-center">
-              <Dropdown
-                className="bg-indigo-700"
-                title='Filter'
-                
-              >
-                <Dropdown.Item
-                  onClick={() => filter()}
+                <Dropdown
+                  className="bg-indigo-700"
+                  title='Filter'
+
                 >
-                  <p className="text-lg font-semibold">Departments</p>
-                  <Dropdown.Submenu>
-                    <Dropdown.Item>
-                      Department 1
-                    </Dropdown.Item>
-                    <Dropdown.Item>
-                      Department 2
-                    </Dropdown.Item>
-                    <Dropdown.Item>
-                      Department 3
-                    </Dropdown.Item>
-                    <Dropdown.Item>
-                      Department 4
-                    </Dropdown.Item>
-                    <Dropdown.Item>
-                      Department 5
-                    </Dropdown.Item>
-                  </Dropdown.Submenu>
-                </Dropdown.Item>
-                <div className="w-32 h-24 relative">
+                  <Dropdown.Item
+                    onClick={() => filter()}
+                  >
+                    <p className="text-lg font-semibold">Departments</p>
+                    <Dropdown.Submenu>
+                      <Dropdown.Item>
+                        Department 1
+                      </Dropdown.Item>
+                      <Dropdown.Item>
+                        Department 2
+                      </Dropdown.Item>
+                      <Dropdown.Item>
+                        Department 3
+                      </Dropdown.Item>
+                      <Dropdown.Item>
+                        Department 4
+                      </Dropdown.Item>
+                      <Dropdown.Item>
+                        Department 5
+                      </Dropdown.Item>
+                    </Dropdown.Submenu>
+                  </Dropdown.Item>
+                  <div className="w-32 h-24 relative">
                     <div className="flex justify-between w-full absolute bottom-0 ml-2">
                       <button className="text-white bg-black h-6 px-2 rounded-sm">Clear</button>
                       <button className="text-white bg-teal-500 h-6 px-2 rounded-sm">Submit</button>
                     </div>
-                </div>
-              </Dropdown>
-               <AiOutlineFilter />
+                  </div>
+                </Dropdown>
+                <AiOutlineFilter />
               </div>
               <button className="px-2 bg-green-600 text-white py-1 flex space-x-1 items-center"><span>Download Pdf</span><AiOutlineFilePdf /></button>
             </div>
@@ -223,9 +235,9 @@ const Career = () => {
         <div className="w-full flex space-x-4 bg-white p-2">
           <div className="w-1/2">
             <h3 className="text-lg">Overall Skill Gap</h3>
-            <div className="w-full flex border rounded-md h-[450px]">
-              <div className="w-1/2">
-                <div className="p-2 w-full h-full">
+            <div className="w-full flex rounded-md h-[450px] drop-shadow-lg">
+              <div className="w-1/2 bg-white">
+                <div className="p-2 bg-white ">
                   <Chart
                     chartType="PieChart"
                     data={data}
@@ -235,7 +247,7 @@ const Career = () => {
                   />
                 </div>
               </div>
-              <div className="w-1/2 p-2 flex justify-center flex-col">
+              <div className="w-1/2 p-2 flex justify-center flex-col bg-white">
                 <div className="p-2">
                   <p className="text-red-500">TEECHNINCAL</p>
                   <ProgressBar progressPercentage={50} />
@@ -257,12 +269,12 @@ const Career = () => {
           </div>
           <div className="w-1/2">
             <h3 className="text-lg">Competency Matrix </h3>
-            <div className="shadow-sm border rounded-md h-[450px] flex">
+            <div className="drop-shadow-lg rounded-md h-[450px] flex">
               <div className="w-3/5 h-full relative">
                 <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20">
                   <p className="text-indigo-500 text-lg font-bold text-center">800<br /><span className="text-sm">USERS</span></p>
                 </div>
-                <div className="p-2 w-full h-full">
+                <div className="p-2 bg-white">
                   <Chart
                     chartType="PieChart"
                     data={datatwo}
@@ -272,20 +284,20 @@ const Career = () => {
                   />
                 </div>
               </div>
-              <div className="w-2/5 flex flex-col justify-center items-center">
+              <div className="w-2/5 flex flex-col justify-center items-center bg-white">
                 <div className="flex space-x-2">
-                    <div className="text-center">
-                      <h4 className="text-4xl font-bold">10</h4>
-                      <button className="bg-red-500 text-white px-2 py-2 rounded-xl w-28 hover:bg-indigo-700">
-                        Taken one Assesment
-                      </button>
-                    </div>
-                    <div className="text-center">
-                      <h4 className="text-4xl font-bold">25</h4>
-                      <button className="text-white bg-green-500 px-2 py-2 rounded-xl w-28 hover:bg-indigo-700">
-                        Taken two Assesment
-                      </button>
-                    </div>
+                  <div className="text-center">
+                    <h4 className="text-4xl font-bold">10</h4>
+                    <button className="bg-red-500 text-white px-2 py-2 rounded-xl w-28 hover:bg-indigo-700">
+                      Taken one Assesment
+                    </button>
+                  </div>
+                  <div className="text-center">
+                    <h4 className="text-4xl font-bold">25</h4>
+                    <button className="text-white bg-green-500 px-2 py-2 rounded-xl w-28 hover:bg-indigo-700">
+                      Taken two Assesment
+                    </button>
+                  </div>
                 </div>
                 <ul className="p-2 text-lg font-semibold mt-6">
                   <li className="text-blue-700 w-44 justify-between flex"><span>Beginner</span> <span className="text-black">10</span></li>
@@ -300,7 +312,7 @@ const Career = () => {
         <div className="w-full flex space-x-4 bg-white p-2">
           <div className="w-1/2">
             <h3 className="text-lg">Attrition Analytics</h3>
-            <div className="shadow-sm border h-[400px]">
+            <div className="drop-shadow-lg h-[400px]">
               <Chart
                 chartType="ComboChart"
                 width="100%"
@@ -312,16 +324,16 @@ const Career = () => {
           </div>
           <div className="w-1/2">
             <h3 className="text-lg"> Leaderboard Analytics </h3>
-            <div className="shadow-sm border rounded-md h-[400px]">
+            <div className="shadow-sm drop-shadow-lg bg-white rounded-md h-[400px]">
               <div className="w-full p-2">
-              <DataTable
+                <DataTable
                   columns={columnstable}
                   data={datatable}
                   customStyles={customStyles}
                   pagination
-              />
+                />
               </div>
-              
+
             </div>
 
           </div>
@@ -329,7 +341,7 @@ const Career = () => {
         <div className="w-full flex space-x-4 bg-white p-2">
           <div className="w-1/2">
             <h3 className="text-lg">Career Path</h3>
-            <div className="shadow-sm border h-[400px] flex items-center">
+            <div className="drop-shadow-lg bg-white h-[400px] flex items-center">
               <div className="flex justify-center items-center flex-wrap gap-4 mt-4 mb-4">
                 <div className="w-48 bg-white drop-shadow-md hover:bg-gray-100">
                   <ul className="text-center font-semibold">
@@ -378,7 +390,7 @@ const Career = () => {
           </div>
           <div className="w-1/2">
             <h3 className="text-lg">Activity</h3>
-            <div className="shadow-sm border rounded-md h-[400px]">
+            <div className="drop-shadow-lg bg-white rounded-md h-[400px]">
               <div className="w-full p-4 flex">
                 <div className="w-40 h-8 bg-pink-500">
                 </div>
